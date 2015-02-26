3762e30247962d861a5ccbdec03354f53539f3f3
BenLLong <ben@benllong.com>
2015-02-26 14:29:36 -0500

    data- vars, dynamic script loading, polish
    Developed into a deployable asset.  Dynamic script loading by Todd.

--- README.md
+++ README.md
@@ -1,4 +1,63 @@
 als_lightbox
 ============
 
-Splash pages, simplified
+Splash pages, simplified.
+
+The code to add to headers of pages:
+-----------
+Implementation for them should be simple. Just cut and paste this code snippet:
+
+    <!-- Add ALS fancybox assets -->
+    <script type="text/javascript"
+        src="//annelewisllc.s3.amazonaws.com/assets/splash/js/als_fancybox_launch.js"
+        data-id="als_fancybox_js"
+        data-max-width="800"
+        data-max-height="450"
+        data-iframe-url="//weta.s3.amazonaws.com/2014_spring_splash/weta-lightbox-spring-src-v1.html"
+        data-start-date="February 26, 2015 05:00:00"
+        data-end-date="March 23, 2015 04:00:00" 
+        data-cookie-name="fancybox_als4"
+        data-cookie-duration="1"
+        data-test-mode="0"
+        />
+    </script>
+
+Note that the client can adjust various options if desired:
+
+    data-id="als_fancybox_js"
+
+just an ID for the script element
+
+    data-max-width="800" 
+
+Max width of the iframe. This is related to the dimensions of the iframe source so shouldn’t be changed on its own.
+
+    data-max-height="450"
+
+Max height of the iframe. This is related to the dimensions of the iframe source so shouldn’t be changed on its own.
+
+    data-iframe-url="//weta.s3.amazonaws.com/2014_spring_splash/weta-lightbox-spring-src-v1.html”
+
+Source URL for the iframe.
+
+    data-start-date="February 26, 2015 05:00:00"
+
+Start time for the splash to be display in UTC. Currently set to the start of the day tomorrow, Feb 26
+
+    data-end-date="March 23, 2015 04:00:00" 
+
+End time for the splash to be display in UTC. Currently set to the start of the day, Mar 23
+
+    data-cookie-name="fancybox_als4"
+
+Name of cookie controlling visibility.
+
+    data-cookie-duration="1"
+
+Cookie expiration duration in days. Currently set to show new visitors the splash once each day.
+
+    data-test-mode="0"
+    
+A value of "1" here activates test mode which always shows the splash regardless of cookies or date.
+
+
---
+++ js/als_fancybox_launch.js
@@ -0,0 +1,230 @@
+// Anne Lewis Strategies
+// Generalized lightbox asset
+// uses fancybox in iframe
+// Developed by by Ben Long <ben@annelewisllc.com>.
+
+// Helper functions. 
+
+function getScript(url, success) {
+
+	var script     = document.createElement('script');
+		script.src = url;
+	
+	var head = document.getElementsByTagName('head')[0],
+	done = false;
+	
+	// Attach handlers for all browsers
+	script.onload = script.onreadystatechange = function() {
+	
+		if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+		
+		done = true;
+			
+			// callback function provided as param
+			success();
+			
+			script.onload = script.onreadystatechange = null;
+			head.removeChild(script);
+			
+		};
+	
+	};
+	
+	head.appendChild(script);
+
+};
+
+function createCookieAls(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookieAls(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function eraseCookieAls(name) {
+	createCookie(name,"",-1);
+}
+
+// Load jQuery if not present. Technique from http://css-tricks.com/snippets/jquery/load-jquery-only-if-not-present/
+
+var thisPageUsingOtherJSLibrary = false;
+
+// Only do anything if jQuery isn't defined
+if (typeof jQuery == 'undefined') {
+
+	if (typeof $ == 'function') {
+		// warning, global var
+		thisPageUsingOtherJSLibrary = true;
+	}
+	
+	getScript('http://code.jquery.com/jquery-latest.min.js', function() {
+	
+		if (typeof jQuery=='undefined') {
+		
+			// Super failsafe - still somehow failed...
+		
+		} else {
+		
+			// jQuery loaded! Make sure to use .noConflict just in case
+
+			jQuery.noConflict();
+			(function( $ ) { 
+				load_fancybox($); 
+			})(jQuery);
+			
+			if (thisPageUsingOtherJSLibrary) {
+
+
+			} else {
+
+
+			}
+		
+		}
+	
+	});
+	
+} else { // jQuery was already loaded
+	
+	load_fancybox($);
+
+};
+
+// Load Fancybox if not present.
+
+function load_fancybox($) {
+
+	if(typeof $.fancybox == 'function') {
+		 load_fancybox_css($);
+		 execute_fancybox($);
+	} else {
+
+		getScript('http://s3.amazonaws.com/annelewisllc/assets/fancybox/jquery.fancybox.pack.js', function() {
+		
+			if (typeof $.fancybox !== 'function') {
+				// Super failsafe - still somehow failed...
+			} else {
+				load_fancybox_css($);
+				execute_fancybox($);
+			}
+		
+		});
+
+	}
+
+}
+
+// Load Fancybox CSS if not present.
+
+function load_fancybox_css($) {
+	if (!$('link[href$="jquery.fancybox.css"]').length) {
+		var fancybox_base_style_tag = document.createElement('link');
+			fancybox_base_style_tag.rel  = 'stylesheet';
+			fancybox_base_style_tag.type = 'text/css';
+			fancybox_base_style_tag.media = 'screen';
+			fancybox_base_style_tag.href = 'http://s3.amazonaws.com/annelewisllc/assets/fancybox/jquery.fancybox.css';
+			fancybox_base_style_tag.id = 'als_fancybox_base_style_tag';
+		document.getElementsByTagName('head')[0].appendChild(fancybox_base_style_tag);
+	}
+}
+
+// Execute fancybox implementation.
+
+function execute_fancybox($) {
+
+	// Add base ALS styling CSS file.
+	var als_style_tag = document.createElement('link');
+		als_style_tag.rel  = 'stylesheet';
+		als_style_tag.type = 'text/css';
+		als_style_tag.media = 'all';
+		als_style_tag.href = '//annelewisllc.s3.amazonaws.com/assets/splash/css/als_fancybox_launch.css';
+	document.getElementsByTagName('head')[0].appendChild(als_style_tag);
+
+	$(document).ready(function( $ ) {
+	
+		$('body').prepend('<div id="als_lightbox" style="display:none;"></div>');
+
+		var todayDate = new Date();
+	
+		//settings
+		var thisScript = document.querySelector('script[data-id="als_fancybox_js"]');
+		var boxMaxWidth = thisScript.getAttribute('data-max-width');
+		var boxMaxHeight = thisScript.getAttribute('data-max-height');
+		var boxUrl = thisScript.getAttribute('data-iframe-url'); //http://localhost/www/github/als_lightbox/weta-lightbox-src.html
+		var alsCookieName = thisScript.getAttribute('data-cookie-name');
+		var alsCookieDuration = thisScript.getAttribute('data-cookie-duration'); //in days
+		var alsStart = thisScript.getAttribute('data-start-date'); //'October 13, 2014 10:00:00'
+		var alsEnd = thisScript.getAttribute('data-end-date'); //'December 13, 2014 23:59:59'
+		var alsTestMode = thisScript.getAttribute('data-test-mode');
+	
+		//defaults
+		if(!alsCookieName) {
+			alsCookieName = "fancybox_als";
+		}
+		if(!alsCookieDuration) {
+			alsCookieDuration = 1;
+		}
+		if(!alsStart) {
+			alsStart = "January 1, 1970 00:00:00";
+		}
+		if(!alsEnd) {
+			alsEnd = "January 1, 2038 00:00:00";
+		}    
+	
+		$("#als_lightbox").fancybox({
+			openEffect: 'fade',
+			closeEffect: 'fade',
+			openSpeed: 500,
+			closeSpeed: 250,	
+			margin: 0,
+			padding: 0,
+			closeBtn: true,
+			autoSize: true,
+			autoScale: false,
+			wrapCSS: 'als_fancybox',
+			scrollOutside: true, //If true, the script will try to avoid horizontal scrolling for iframes and html content 
+			maxHeight: boxMaxHeight,
+			maxWidth: boxMaxWidth,
+			href: boxUrl,
+			type: 'iframe',
+			helpers: {
+				overlay: {
+					locked: true,
+					closeClick: true
+				}
+			},
+			tpl: {
+				wrap     : '<div class="als-fancybox-wrap fancybox-wrap" tabIndex="-1"><div class="als-fancybox-skin fancybox-skin"><div class="fancybox-outer"><div class="fancybox-inner als-fancybox-inner"></div></div></div></div>',
+				closeBtn : '<a title="Close" class="fancybox-item als-fancybox-close fancybox-close" href="javascript:;"></a>'
+			}	
+		});
+	
+	
+		if(
+			(!readCookieAls(alsCookieName)
+			&& todayDate > new Date(alsStart)
+			&& todayDate < new Date(alsEnd)
+		) || (
+			alsTestMode >= 1
+		)) {
+			$("#als_lightbox").trigger('click');
+			createCookieAls(alsCookieName,1,alsCookieDuration);
+		}
+	
+	});
+
+}
